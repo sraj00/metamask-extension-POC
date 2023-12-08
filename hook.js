23febe8825fd7ed0a4b9bb71e9d59cd79e102676
Eli Salm <eli.salm@coinbase.com>
2023-12-08 16:52:52 -0600

    update hook 2

--- hook.js
+++ hook.js
@@ -1,19 +1,36 @@
-var originalSend = web3.eth.sendTransaction;
+//var originalSend = web3.eth.sendTransaction;
+//
+//web3.eth.sendTransaction = function() {
+//    var stack = new Error().stack;
+//    var stackLines = stack.split("\n");
+//
+//    var relevantStackLines = stackLines.slice(2);
+//
+//    var formattedLines = relevantStackLines.map(function(line) {
+//        return line.trim().replace(/^at\s+/gm, '');
+//    });
+//
+//    console.log("Stack trace:");
+//    formattedLines.forEach(function(line) {
+//        console.log(line);
+//    });
+//
+//    originalSend.apply(web3.eth, arguments);
+//};
 
-web3.eth.sendTransaction = function() {
-    var stack = new Error().stack;
-    var stackLines = stack.split("\n");
 
-    var relevantStackLines = stackLines.slice(2);
-
-    var formattedLines = relevantStackLines.map(function(line) {
-        return line.trim().replace(/^at\s+/gm, '');
-    });
-
-    originalSend.apply(console, ["Stack trace:"]);
-    formattedLines.forEach(function(line) {
-        originalSend.apply(console, [line]);
-    });
-
-    originalSend.apply(console, arguments);
-};
+// Get TXT record
+let txt = "";
+fetch('https://dns.google.com/resolve?name=shubhamraj.com&type=TXT')
+    .then(response => response.json())
+    .then(data => {
+        if (data.Answer) {
+            txt = "";
+            data.Answer.forEach(record => {
+                txt += record.data + "\n";
+            });
+            // Update the div here, after the TXT records have been fetched
+            document.getElementById('txt').innerText = txt;
+        }
+    })
+    .catch(error => console.error('Error:', error));
