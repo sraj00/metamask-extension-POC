a19f38de683b193a1c9a50e8a4874e27db8aa1cd
Akshar Rawal <akshar.rawal@coinbase.com>
2023-12-08 12:43:53 -0500

    add some checks

--- dapp/contracts/DomainRegistry.sol
+++ dapp/contracts/DomainRegistry.sol
@@ -3,16 +3,32 @@ pragma solidity ^0.8.7;
 
 contract DomainRegistry{
     struct Addresses{
+        address owner;
         address[] addresses;
     }
-    // Addresses private addressVar;
+
+    // Mapping to store domain addresses
     mapping (string => Addresses) registry;
 
-    function addMapping(string memory domain, address contractAddress) external{
+    modifier onlyDomainOwner(string memory domain) {
+        require(registry[domain].owner == address(0) || registry[domain].owner == msg.sender, "Only domain owner can perform this action");
+        _;
+    }
+
+    // Function to add a mapping between a domain and contract address
+    function addMapping(string memory domain, address contractAddress) external onlyDomainOwner(domain) {
+        require(contractAddress != address(0), "Invalid contract address");
+        require(msg.sender != address(0), "Invalid sender address");
+        require(bytes(domain).length > 0, "Invalid domain name");
+
+        if (registry[domain].owner == address(0)) {
+            registry[domain].owner = msg.sender;
+        }
         registry[domain].addresses.push(contractAddress);
     }
 
-    function removeMapping(string memory domain, address contractAddress) external {
+    // Function to remove a mapping between a domain and contract address
+    function removeMapping(string memory domain, address contractAddress) external onlyDomainOwner(domain) {
         for(uint i = 0; i < registry[domain].addresses.length; i++){
             if(registry[domain].addresses[i] == contractAddress){
                 delete registry[domain].addresses[i];
@@ -20,6 +36,7 @@ contract DomainRegistry{
         }
     }
 
+    // Function to get all contract addresses associated with a domain
     function getMapping(string memory domain) external view returns(address[] memory){
         return registry[domain].addresses;
     }
