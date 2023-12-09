275b27fbcc28f38a31c0a54f5850625877927204
Akshar Rawal <akshar.rawal@coinbase.com>
2023-12-08 19:57:05 -0500

    deployment readiness

--- dapp/hardhat.config.js
+++ dapp/hardhat.config.js
@@ -1,11 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
 require("./tasks/faucet");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.7",
+  networks: {
+    base_goerli: {
+      url: `https://base-goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
